c627f0bed074e5ea30c5c707ba0b0cc1ba5733c9
nambhhe <nambhhe176453@fpt.edu.vn>
2025-02-02 22:21:45 +0700

    deleted ipv4 and retest server

--- Backend/src/api/middlewares/errorHandlers.js
+++ Backend/src/api/middlewares/errorHandlers.js
@@ -1,10 +1,10 @@
-import createError from 'http-errors';
+import createError from "http-errors";
 
 // Middleware bắt lỗi 400 (Bad Request)
 export const handleBadRequest = (req, res, next) => {
     const badRequestCondition = false; // Thay đổi theo điều kiện của bạn
     if (badRequestCondition) {
-        next(createError(400, 'Bad Request'));
+        next(createError(400, "Bad Request"));
     } else {
         next();
     }
@@ -12,7 +12,7 @@ export const handleBadRequest = (req, res, next) => {
 
 // Middleware bắt lỗi 404 (Not Found)
 export const handleNotFound = (req, res, next) => {
-    next(createError(404, 'Not Found'));
+    next(createError(404, "Not Found yadiyadi yada"));
 };
 
 // Middleware xử lý các lỗi khác (500, Internal Server Error)
@@ -21,7 +21,7 @@ export const handleServerErrors = (err, req, res, next) => {
     res.json({
         error: {
             status: err.status || 500,
-            message: err.message
-        }
+            message: err.message,
+        },
     });
 };
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,33 +7,8 @@ import AvailabilityController from "./api/controllers/availability.controller.js
 // Tải biến môi trường từ .env file
 dotenv.config();
 
-function getPhysicalIPv4() {
-    const networkInterfaces = os.networkInterfaces();
-    const ignoredAdapters = ["vEthernet", "VirtualBox", "docker", "TAP-Windows", "Tailscale", "Loopback"];
-
-    for (const interfaceName in networkInterfaces) {
-        // Bỏ qua các adapter ảo như 'vEthernet', 'docker', 'VirtualBox', 'TAP-Windows', 'Tailscale', 'Loopback', v.v.
-        if (ignoredAdapters.some((adapter) => interfaceName.includes(adapter))) {
-            continue;
-        }
-
-        const interfaces = networkInterfaces[interfaceName];
-        for (const iface of interfaces) {
-            // Chỉ lấy IPv4 và bỏ qua các interface nội bộ (localhost)
-            if (iface.family === "IPv4" && !iface.internal) {
-                return iface.address;
-            }
-        }
-    }
-
-    return null; // Trả về null nếu không tìm thấy địa chỉ IP
-}
-
-const localIP = getPhysicalIPv4();
-console.log("IP: ", localIP);
-
 const PORT = process.env.PORT;
-const HOSTNAME = localIP || "localhost"; // Sử dụng "localhost" nếu localIP là null hoặc undefined
+const HOSTNAME = "localhost"; // Sử dụng "localhost"
 
 // Ensure MongoDB is connected before starting the server
 const startServer = async () => {
